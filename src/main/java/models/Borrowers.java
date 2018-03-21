c7d27b402b8fba4fddc9f425ae5f72169ab3afb4
Graeme Brough <gdbrough@blueyonder.co.uk>
2018-03-21 15:03:18 +0000

    builds out models.books and models.borrowers and maps DBs in cfg

--- src/main/java/models/Borrowers.java
+++ src/main/java/models/Borrowers.java
@@ -1,4 +1,50 @@
 package models;
 
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "borrowers")
 public class Borrowers {
+
+    private int id;
+    private String name;
+    private Set<Books> itemsBorrowed;
+
+    public Borrowers() {
+    }
+
+    public Borrowers(String name) {
+        this.name = name;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Column(name = "name")
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "book_id", nullable = false)
+    public Set<Books> getItemsBorrowed() {
+        return itemsBorrowed;
+    }
+
+    public void setItemsBorrowed(Set<Books> itemsBorrowed) {
+        this.itemsBorrowed = itemsBorrowed;
+    }
 }
