87530e7bc538e6872ef5b5c948be787bf13f07f7
Paul Stewart <stewartpaul22@yahoo.co.uk>
2018-03-21 15:20:53 +0000

    Adds get(/books) route to BooksController

--- src/main/java/controllers/BooksController.java
+++ src/main/java/controllers/BooksController.java
@@ -1,9 +1,37 @@
 package controllers;
 
+import db.DBHelper;
+import models.Books;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static spark.Spark.get;
+import static spark.Spark.post;
+
 public class BooksController {
 
-    public static void main(String[] args) {
-
+    public BooksController() {
+        this.setupEndpoints();
     }
 
+    private void setupEndpoints() {
+
+        get("/books", (req, res) -> {
+
+            List<Books> books = DBHelper.getAll(Books.class);
+
+            Map<String, Object> model = new HashMap<>();
+            model.put("template", "templates/books/index.vtl");
+            model.put("books", books);
+
+            return new ModelAndView(model, "templates/layout.vtl");
+
+
+        }, new VelocityTemplateEngine());
+
+    }
 }
--- src/main/java/controllers/BorrowersController.java
+++ src/main/java/controllers/BorrowersController.java
@@ -1,4 +1,9 @@
 package controllers;
 
 public class BorrowersController {
+
+    public static void main(String[] args) {
+
+    }
+
 }
